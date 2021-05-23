41282c8a29d0177bce44ab7b628d7af71c15a472
abolzzz <82838037+abolzzz@users.noreply.github.com>
2021-05-23 19:19:46 +0300

    Add files via upload

---
+++ md6/app.js
@@ -0,0 +1,8 @@
+function funk() {
+    let menu = document.getElementById("menu");
+    let links = document.getElementById("links");
+
+    menu.classList.toggle("ham-menu")
+    links.classList.toggle("ham-links")
+    links.classList.toggle("links-links")
+};
